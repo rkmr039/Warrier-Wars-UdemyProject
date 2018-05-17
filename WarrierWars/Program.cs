f3e437f64f43fc13b55c7b8a0a306a1af5a13fed
RishabKumar <rkmr039@gmail.com>
2018-05-17 18:29:54 +0530

    Add project files.

---
+++ WarrierWars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WarrierWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
